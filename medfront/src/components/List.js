481b019b34349f841cfd38a0ef0a21edaf606b8e
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-07-02 13:24:29 +0500

    main page better

--- medfront/src/components/List.js
+++ medfront/src/components/List.js
@@ -1,23 +1,25 @@
+import axios from 'axios'
 import React from 'react'
 import {AiOutlineMinusCircle, AiOutlineDelete} from 'react-icons/ai'
 
 export default function List(props) {
+
   return (
-    <>
+    <li>
     <div className='list'>
       <div className='medName'>
       <h2>{props.name}</h2>
       </div>
       <div className='desc'>
-        <p>{props.email}</p>
+        <p>{props.description}</p>
       </div>
       <div className='buttons'>
       <button className='minus'>Minus</button>
-      <button className='delete'>Delete</button>
+      <button className='delete' onClick={()=>props.handleDelete(props.id)}>Delete</button>
       <button className='update'>Update</button>
       </div>
     </div>
-    </>
+    </li>
   )
 }
 
--- medfront/src/pages/Main.js
+++ medfront/src/pages/Main.js
@@ -6,22 +6,35 @@ import axios from "axios";
 
 export default function Main(){
     const [meds,setMeds]=useState()
+    const token=`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsNHY5cmpyODAwMDI1NHVpazE0N2s3YnIiLCJpYXQiOjE2NTY3NDc2OTIsImV4cCI6MTY1Njc1MTI5Mn0.cp4bHctE8X7elsGKEn-KbPdTW0ntb1mXwlt3PJhjyOg`//will get from local storage or cookies but this is for testing
     useEffect(()=>{
         const result = async()=>{
-            const res=await axios("https://randomuser.me/api/")//just for testing
-            setMeds(res.data.results)
+            const res=await axios.get("http://localhost:5000/api/medicines/",{headers:{"Authorization":`${token}`}})//just for testing
+            setMeds(res.data)
+            console.log(res.data)
+
         }
         result();
-    },[])
+    },[meds])
+    
+  function handleDelete(id){
+    const headers={
+      'Authorization':`${token}`,
+    }
+    axios.delete(`http://localhost:5000/api/medicines/${id}`,{headers}).then(()=>console.log("Success"))
+
+  }
     console.log(meds)
     const medList=meds?.map((med)=>{
-        return <List id={med.id.value} name={med.name.first} email={med.email}/>//just for testing will change the props later when using our real db
+        return <List id={med.id} name={med.name} description={med.description} token={token} handleDelete={handleDelete}/>//just for testing will change the props later when using our real db
     })
     return(
         <div>
             <Header/>
             <div className="list-cont">
-                {medList}
+                <ul>
+                    {medList}
+                </ul>
             </div>
         </div>
     )
